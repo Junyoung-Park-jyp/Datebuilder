72060b671b50f3cb1e2cc730aedaa190f28567d2
raypark1241 <raypark1241@gmail.com>
2023-09-12 11:25:32 +0900

    food,place,cafe모달제작

--- single_pages/admin.py
+++ single_pages/admin.py
@@ -8,6 +8,10 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'content')
+    list_filter = ('subject', 'content')
+    search_fields = ('subject', 'content')
 admin.site.register(Food,MarkdownxModelAdmin )
 admin.site.register(Cafe, MarkdownxModelAdmin )
 admin.site.register(Place, MarkdownxModelAdmin )
@@ -16,3 +20,4 @@ admin.site.register(Post, MarkdownxModelAdmin )
 #admin페이지에 카테고리, Tags 추가하기
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Tag,TagAdmin)
+admin.site.register(Course)
--- single_pages/views.py
+++ single_pages/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import *
 from django.http import JsonResponse
 from django.views.generic import *
+from django.http import JsonResponse
+import json
 
 # 포스트 연결하기 테스트
 from single_pages.models import Post
@@ -61,22 +63,63 @@ class PlaceList(ListView):
     template_name = "single_pages/place.html"
     context_object_name = "places"
 
-def create_course(request):
-    if request.method == 'GET':
-        # 클라이언트에서 전달한 데이터 ID 가져오기
-        data_id = request.GET.get('data_id')
+# def create_course(request):
+#     if request.method == 'GET':
+#         # 클라이언트에서 전달한 데이터 ID 가져오기
+#         data_id = request.GET.get('data_id')
 
-        # 각 모델에서 해당 데이터 조회
-        selected_food = get_object_or_404(Food, pk=data_id)
-        selected_cafe = get_object_or_404(Cafe, pk=data_id)
-        selected_place = get_object_or_404(Place, pk=data_id)
+#         # 각 모델에서 해당 데이터 조회
+#         selected_food = get_object_or_404(Food, pk=data_id)
+#         selected_cafe = get_object_or_404(Cafe, pk=data_id)
+#         selected_place = get_object_or_404(Place, pk=data_id)
+
+#         # Course 모델에 데이터 저장
+#         course = Course(
+#             subject=f"{selected_food.subject}, {selected_cafe.subject}, {selected_place.subject}",
+#             content=f"{selected_food.content}, {selected_cafe.content}, {selected_place.content}"
+#         )
+#         course.save()
+
+#         # JSON 응답 반환
+#         response_data = {
+#             'message': 'Course created successfully!',
+#             'course_id': course.pk  # 새로 생성된 Course의 ID 반환
+#         }
+#         return JsonResponse(response_data)
+
+#     return render(request, 'build/create_course.html')
+
+from django.http import JsonResponse
+import json
+
+def create_course(request):
+    if request.method == 'POST':
+        # 클라이언트에서 전달한 데이터 ID 배열 가져오기
+        data_ids = json.loads(request.body)
+
+        # 선택한 데이터 ID 배열을 저장할 리스트
+        selected_data = []
+
+        # 각 데이터 ID를 순회하며 객체를 조회하고 selected_data에 추가
+        for data_id in data_ids:
+            selected_cafe = get_object_or_404(Cafe, pk=data_id)
+            selected_food = get_object_or_404(Food, pk=data_id)
+            selected_place = get_object_or_404(Place, pk=data_id)
+
+            selected_data.append({
+                'cafe_subject': selected_cafe.subject,
+                'cafe_content': selected_cafe.content,
+                'food_subject': selected_food.subject,
+                'food_content': selected_food.content,
+                'place_subject': selected_place.subject,
+                'place_content': selected_place.content,
+            })
 
         # Course 모델에 데이터 저장
-        course = Course(
-            subject=f"{selected_food.subject}, {selected_cafe.subject}, {selected_place.subject}",
-            content=f"{selected_food.content}, {selected_cafe.content}, {selected_place.content}"
+        course = Course.objects.create(
+            subject=", ".join([f"{data['cafe_subject']}, {data['food_subject']}, {data['place_subject']}" for data in selected_data]),
+            content=", ".join([f"{data['cafe_content']}, {data['food_content']}, {data['place_content']}" for data in selected_data]),
         )
-        course.save()
 
         # JSON 응답 반환
         response_data = {
@@ -85,4 +128,4 @@ def create_course(request):
         }
         return JsonResponse(response_data)
 
-    return render(request, 'build/create_course.html')
+    return render(request, 'single_pages/create_course.html')

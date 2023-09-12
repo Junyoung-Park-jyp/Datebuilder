06ce0e1c1a372bdfe084859a8fa4a87b919a9930
KweKwon <aarnlrnjsaa@naver.com>
2023-09-13 00:21:56 +0900

    지도 라인이어가기 우클릭으로 변경, 서치 네브바 디자인 변경, 지도 정보창, 클릭시 마크이동 추가

--- static/single_pages/js/map.js
+++ static/single_pages/js/map.js
@@ -1,8 +1,8 @@
 // 마커표시
 var position = new naver.maps.LatLng(37.5112, 127.0981);
-var HOME_PATH = window.HOME_PATH || ".";
+var HOME_PATH = window.HOME_PATH || '.';
 
-var map = new naver.maps.Map("map", {
+var map = new naver.maps.Map('map', {
   center: position,
   zoom: 15,
 });
@@ -19,22 +19,13 @@ var menuLayer = $(
 
 map.getPanes().floatPane.appendChild(menuLayer[0]);
 
-naver.maps.Event.addListener(map, "click", function (e) {
-  // 클릭라인 연결하기
-  var point = e.coord;
-  var path = polyline.getPath();
-  path.push(point);
-
-  var marker = new naver.maps.Marker({
-    position: e.coord,
-    map: map,
-  });
-
+// 클릭이벤트
+naver.maps.Event.addListener(map, 'click', function (e) {
   markerList.push(marker);
-  marker.setPosition(point);
+  marker.setPosition(e.coord);
 });
-
-naver.maps.Event.addListener(map, "keydown", function (e) {
+// 키다운 이벤트
+naver.maps.Event.addListener(map, 'keydown', function (e) {
   var keyboardEvent = e.keyboardEvent,
     keyCode = keyboardEvent.keyCode || keyboardEvent.which;
 
@@ -48,18 +39,17 @@ naver.maps.Event.addListener(map, "keydown", function (e) {
     }
 
     markerList = [];
-
     menuLayer.hide();
   }
 });
-
-naver.maps.Event.addListener(map, "mousedown", function (e) {
+// 마우스다운이벤트
+naver.maps.Event.addListener(map, 'mousedown', function (e) {
   menuLayer.hide();
 });
 
-naver.maps.Event.addListener(map, "rightclick", function (e) {
+naver.maps.Event.addListener(map, 'rightclick', function (e) {
   var coordHtml =
-    "Coord: " + "(우 클릭 지점 위/경도 좌표)" + "<br />" + "Point: " + e.point + "<br />" + "Offset: " + e.offset;
+    'Coord: ' + '(우 클릭 지점 위/경도 좌표)' + '<br />' + 'Point: ' + e.point + '<br />' + 'Offset: ' + e.offset;
 
   menuLayer
     .show()
@@ -69,36 +59,56 @@ naver.maps.Event.addListener(map, "rightclick", function (e) {
     })
     .html(coordHtml);
 
-  console.log("Coord: " + e.coord.toString());
+  console.log('Coord: ' + e.coord.toString());
+
+  // 마커표시 이동
+
+  // 클릭라인 연결하기
+  var point = e.coord;
+  var path = polyline.getPath();
+  path.push(point);
+
+  var marker = new naver.maps.Marker({
+    position: e.coord,
+    map: map,
+  });
+  marker.setPosition(point);
 });
 
 // 클릭라인 연결하기
 var polyline = new naver.maps.Polyline({
   map: map,
   path: [],
-  strokeColor: "#82d8d9",
+  strokeColor: '#82d8d9',
   strokeWeight: 5,
 });
 
 // 정보창
-var contentString = ["#정보 넣을곳"].join("");
+var contentString = [
+  '<div class="iw_inner">',
+  '   <h3>{ 가게명 }</h3>',
+  '   <p>{주소}<br />',
+  '       <img src="' + HOME_PATH + '/img/example/.jpg" width="55" height="55" alt="" class="thumb" /><br />',
+  '       {전화번호} | { 종류 }<br />',
+  '       <a href="http://www.seoul.go.kr" target="_blank">{사이트}</a>',
+  '   </p>',
+  '</div>',
+].join('');
 var infowindow = new naver.maps.InfoWindow({
   content: contentString,
-  maxWidth: 140,
-  backgroundColor: "#f5f5f5",
-  borderColor: "#82d8d9",
+  maxWidth: 400,
+  backgroundColor: '#f5f5f5',
+  borderColor: '#82d8d9',
   borderWidth: 5,
-  anchorSize: new naver.maps.Size(30, 30),
-  anchorSkew: true,
-  anchorColor: "#55555",
+  anchorSize: new naver.maps.Size(30, 50),
+  anchorSkew: false,
+  anchorColor: '#82d8d9',
   pixelOffset: new naver.maps.Point(20, -20),
 });
-naver.maps.Event.addListener(marker, "click", function (e) {
+naver.maps.Event.addListener(marker, 'click', function (e) {
   if (infowindow.getMap()) {
     infowindow.close();
   } else {
     infowindow.open(map, marker);
   }
 });
-
-// 마커표시 이동

9d489616f7d3bfeda761d7a6830564cdec9097ef
raypark1241 <raypark1241@gmail.com>
2023-09-13 11:05:47 +0900

    d

--- date_builder/settings.py
+++ date_builder/settings.py
@@ -56,8 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # allauth.account.middleware.AccountMiddleware
     'allauth.account.middleware.AccountMiddleware',
+    'allauth.account.middleware.AuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'date_builder.urls'

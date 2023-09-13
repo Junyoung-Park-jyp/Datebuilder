3981d98fc0132dcd004f20088cfb303b8fc8ee74
raypark1241 <raypark1241@gmail.com>
2023-09-13 11:18:39 +0900

    9.13병합완료

--- date_builder/settings.py
+++ date_builder/settings.py
@@ -56,8 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
-    'allauth.account.middleware.AuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'date_builder.urls'
